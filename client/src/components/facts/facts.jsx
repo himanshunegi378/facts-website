70d474de00f8cc266cfef0da8740fe66162110d3
himansunegi378 <davidxavier378@gmail.com>
2020-02-02 18:24:00 +0530

    checked page rendering not imp

--- client/src/components/facts/facts.jsx
+++ client/src/components/facts/facts.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./style.module.css";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 let ToggleBookmark = props => {
   return (
@@ -49,6 +50,7 @@ class facts extends Component {
   }
 
   componentDidMount() {
+
     axios.get("/api/users/fetch_facts").then(res => {
       this.setState({ facts: res.data });
     });
@@ -57,7 +59,10 @@ class facts extends Component {
     let item = this.state.facts.map(fact => {
       return <Item key={fact._id} _id={fact._id} value={fact.fact} />;
     });
-    return <div className='container section'>{item}</div>;
+    return <div className='container section'>
+      <div className='card z-depth-0 right-align '> <Link className='btn hoverable blue darken-1' to='/bookmarks'>Bookmarks</Link></div>
+      {item}
+      </div>;
   }
 }
 
